8321ab7bcb926c67742da128cc6e8788a96d2112
sizonenko.artyom <sizonenko.artyom@yandex.ru>
2024-12-29 21:34:49 +0300

    some commit

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/greet', methods=['GET'])
 def greet():
-    return jsonify(message="hi")
+    return jsonify(message="hi for you")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
