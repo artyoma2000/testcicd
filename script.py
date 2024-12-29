52c202aede953a3387b157b6f47741aa646ec5c0
sizonenko.artyom <sizonenko.artyom@yandex.ru>
2024-12-29 22:14:46 +0300

    added new metod

--- script.py
+++ script.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def greet():
     return jsonify(message="hi for you")
 
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify(message="test answer")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
